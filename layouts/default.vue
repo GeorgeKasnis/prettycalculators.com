16ba5e335ad50aa273149d15db70a01d5d1b4506
gkas <george.kasnis@gmail.com>
2023-03-18 23:22:57 +0200

    wip

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,34 +1,43 @@
 <template>
-  <div class="w-screen h-screen flex items-center flex-col border border-black">
-    <UiTheHeader />
-    <div class="flex-1 grid place-items-center w-full">
-      <slot />
+    <div class="w-screen full-height flex items-center flex-col border border-black">
+        <UiTheHeader />
+        <div class="flex-1 grid place-items-center w-full">
+            <slot />
+        </div>
+        <UiTheFooter />
     </div>
-    <UiTheFooter />
-  </div>
 </template>
 
 <script>
 export default {
-  mounted() {
-    let favicons = ["/B.png", "/M.png", "/I.png"];
-    let counter = 0;
+    mounted() {
+        let vh = window.innerHeight;
 
-    let link = document.querySelector("link[rel*='icon']") || document.createElement("link");
-    link.type = "image/x-icon";
-    link.rel = "shortcut icon";
-    link.href = favicons[0];
-    document.getElementsByTagName("head")[0].appendChild(link);
+        if (document.documentElement.clientHeight < window.innerHeight) {
+            const offset = window.innerHeight - document.documentElement.clientHeight;
+            vh += offset;
+        }
 
-    setInterval(function () {
-      let link = document.querySelector("link[rel*='icon']") || document.createElement("link");
-      link.type = "image/x-icon";
-      link.rel = "shortcut icon";
-      link.href = favicons[counter];
-      document.getElementsByTagName("head")[0].appendChild(link);
-      if (counter == favicons.length - 1) counter = 0;
-      else counter++;
-    }, 2500);
-  },
+        document.querySelector(".full-height").style.height = `${vh}px`;
+
+        let favicons = ["/B.png", "/M.png", "/I.png"];
+        let counter = 0;
+
+        let link = document.querySelector("link[rel*='icon']") || document.createElement("link");
+        link.type = "image/x-icon";
+        link.rel = "shortcut icon";
+        link.href = favicons[0];
+        document.getElementsByTagName("head")[0].appendChild(link);
+
+        setInterval(function () {
+            let link = document.querySelector("link[rel*='icon']") || document.createElement("link");
+            link.type = "image/x-icon";
+            link.rel = "shortcut icon";
+            link.href = favicons[counter];
+            document.getElementsByTagName("head")[0].appendChild(link);
+            if (counter == favicons.length - 1) counter = 0;
+            else counter++;
+        }, 2500);
+    },
 };
 </script>
