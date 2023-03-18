ef5afbf75bbed0a3fe00a7bf4adfcd373f98eb9c
gkas <george.kasnis@gmail.com>
2023-03-18 18:18:52 +0200

    wip

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,29 @@
 <template>
   <div class="w-full p-small">
-    <div class="max-w-xl mx-auto w-full flex flex-col gap-small relative">
-      <div class="">Calculate your BMI</div>
-      <div class="flex items-center border border-black">
-        <v-text-field placeholder="Height" v-model="height" :rules="[positiveNumberRule, requiredRule]" outlined hide-details="true" type="number"></v-text-field>
-        <div class="p-small bg-black text-white w-16 grid place-items-center">Cm</div>
+    <div class="max-w-xl mx-auto w-full">
+      <div class=" flex flex-col gap-small relative">
+        <div class="">Calculate your BMI</div>
+        <div class="flex items-center border border-black">
+          <v-text-field placeholder="Height" v-model="height" :rules="[positiveNumberRule, requiredRule]" outlined hide-details="true" type="number"></v-text-field>
+          <div class="p-small bg-black text-white w-16 grid place-items-center">Cm</div>
+        </div>
+        <div class="flex items-center border border-black">
+          <v-text-field placeholder="Weight" v-model="weight" outlined :rules="[positiveNumberRule, requiredRule]" hide-details="true" type="number" class=""> </v-text-field>
+          <div class="p-small bg-black text-white w-16 grid place-items-center">Kg</div>
+        </div>
+        <button @click="calculateBmi" @keyup.enter="calculateBmi" class="h-14 bg-black duration-300 transition-all">Go</button>
+        <div
+          class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-white bg-black w-full h-full grid place-content-center clip-path-circle"
+          :class="bmi ? 'active' : ''"
+        >
+          <p v-if="bmi">Your BMI is {{ bmi.toFixed(2) }}</p>
+          <p v-if="bmi">You are {{ bmiCategory }}</p>
+          <button @click="clearEverything" class="absolute right-2 top-2 h-4 w-4 z-10">x</button>
+        </div>
       </div>
-      <div class="flex items-center border border-black">
-        <v-text-field placeholder="Weight" v-model="weight" outlined :rules="[positiveNumberRule, requiredRule]" hide-details="true" type="number" class=""> </v-text-field>
-        <div class="p-small bg-black text-white w-16 grid place-items-center">Kg</div>
-      </div>
-      <button @click="calculateBmi" @keyup.enter="calculateBmi" class="h-14 bg-black  duration-300 transition-all">Go</button>
-      <div  class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-white bg-black w-full h-full grid place-content-center clip-path-circle" :class="bmi ? 'active': ''">
-        <p v-if="bmi">Your BMI is {{ bmi.toFixed(2) }}</p>
-        <p v-if="bmi">You are {{ bmiCategory }}</p>
-        <button @click="clearEverything" class="absolute right-2 top-2 h-4 w-4 z-10">x</button>
-      </div>
+      <transition name="fade-in">
+        <div class="text-red-700 h-14 mt-small transition-all duration-300" :class="errors ? 'opacity-100':'opacity-0'">{{ errors }}</div>
+      </transition>
     </div>
   </div>
 </template>
@@ -27,6 +35,7 @@ export default {
       height: null,
       weight: null,
       bmi: null,
+      errors: "",
     };
   },
   computed: {
@@ -55,19 +64,26 @@ export default {
   },
   methods: {
     calculateBmi() {
-      if (this.height && this.weight) {
+      if (!this.height || !this.weight) {
+        this.errors = "All fields is required";
+        this.bmi = null;
+      } else if (this.height <= 0 || this.weight <= 0) {
+        this.errors = "All fields must be positive numbers";
+        this.bmi = null;
+      } else if (this.height && this.weight) {
         const heightInMeters = this.height / 100;
         const bmi = this.weight / (heightInMeters * heightInMeters);
         this.bmi = bmi;
+        this.errors = "";
       } else {
         this.bmi = null;
       }
     },
-    clearEverything(){
+    clearEverything() {
       this.bmi = null;
       this.height = null;
       this.weight = null;
-    }
+    },
   },
 };
 </script>
