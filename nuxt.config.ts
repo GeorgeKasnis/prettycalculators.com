8ae83d88daab5deaca5bff5cf74073873f78eefc
gkas <george.kasnis@gmail.com>
2023-03-18 20:14:03 +0200

    wip

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,34 +1,37 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  },
-
-  css: ["~/assets/css/index.css", "vuetify/lib/styles/main.sass"],
-
-  app: {
-    head: {
-      meta: [
-        {
-          name: "viewport",
-          content: "width=device-width, initial-scale=1",
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
         },
-        {
-          charset: "utf-8",
-        },
-      ],
     },
-  },
 
-  build: {
-    transpile: ["vuetify"],
-  },
-  vite: {
-    define: {
-      "process.env.DEBUG": false,
+    css: ["~/assets/css/index.css", "vuetify/lib/styles/main.sass"],
+
+    app: {
+        head: {
+            meta: [
+                {
+                    name: "viewport",
+                    content: "width=device-width, initial-scale=1",
+                },
+                {
+                    charset: "utf-8",
+                },
+            ],
+            htmlAttrs: {
+                lang: "en",
+            },
+        },
     },
-  },
+
+    vite: {
+        define: {
+            "process.env.DEBUG": false,
+        },
+    },
+
+    
+    
 });
