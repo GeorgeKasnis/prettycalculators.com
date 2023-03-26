6607df50b47ed8f37b14782f29d8e74d93b64413
gkas <george.kasnis@gmail.com>
2023-03-26 14:08:53 +0300

    wip

--- components/ui/BaseContainer.vue
+++ components/ui/BaseContainer.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="max-w-lg w-full">
+    <div class="max-w-lg w-full p-small">
         <slot />
     </div>
 </template>
--- components/ui/FormContainer.vue
+++ components/ui/FormContainer.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex flex-col gap-small relative w-full max-w-lg m-small">
+    <div class="flex flex-col gap-small relative w-full max-w-lg ">
         <div>{{title}}</div>
         <slot />
     </div>
