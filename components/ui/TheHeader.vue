915aa2ddfb8ca91e2d6d047870092776f81d4c5f
gkas <george.kasnis@gmail.com>
2023-03-29 21:51:00 +0300

    wip

--- components/ui/TheHeader.vue
+++ components/ui/TheHeader.vue
@@ -1,14 +1,35 @@
 <template>
-    <header class="p-small w-full fixed top-0 left-0 z-50 h-16 flex justify-between items-center bg-[#fafad2] border-b-2 border-black mb-huge">
+    <header class="p-small w-full fixed top-0 left-0 z-50 h-16 flex justify-between items-center bg-[#fafad2] border-b-2 border-transparent mb-huge transition-all" :class="isScrolled ? '!border-black' : ''">
         <div class="flex justify-between w-full">
             <div>
                 <nuxt-link to="/">Pretty Calculators</nuxt-link>
             </div>
             <nav class="child:mx-2">
-                <NuxtLink activeClass="active-link"  to="/fitness">Fitness</NuxtLink>
-                <NuxtLink activeClass="active-link"  to="/math">Math</NuxtLink>
-                <NuxtLink activeClass="active-link"  to="/unit">Unit</NuxtLink>
+                <NuxtLink activeClass="active-link" to="/fitness">Fitness</NuxtLink>
+                <NuxtLink activeClass="active-link" to="/math">Math</NuxtLink>
+                <NuxtLink activeClass="active-link" to="/unit">Unit</NuxtLink>
             </nav>
         </div>
     </header>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            isScrolled: false,
+        };
+    },
+    mounted() {
+        window.addEventListener("scroll", this.handleScroll);
+    },
+    methods: {
+        handleScroll() {
+            this.isScrolled = window.scrollY > 10 ? true : false;
+        },
+    },
+    beforeUnmount() {
+        window.removeEventListener("scroll", this.handleScroll);
+    },
+};
+</script>
