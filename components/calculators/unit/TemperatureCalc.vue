dccd4d936cf2f2a87c6a1edffeebbb695a0fc843
gkas <george.kasnis@gmail.com>
2023-03-29 22:43:33 +0300

    wip

--- components/calculators/unit/TemperatureCalc.vue
+++ components/calculators/unit/TemperatureCalc.vue
@@ -10,6 +10,17 @@
                 </div>
             </UiFormContainer>
         </UiFormErrorContainer>
+
+        <UiFormErrorContainer :errors="form.fahrenheitError">
+            <UiFormContainer>
+                <InputsTextInput aria-label="Temperature in Fahrenheit" placeholder="Temperature in Fahrenheit" v-model="form.fahrenheit" measurementUnit="&#8457;" />
+                <ButtonsCalcBtn @click="calculateFahrenheit" @keyup.enter="calculateFahrenheit" />
+                <div class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-white bg-black w-full h-full grid place-content-center clip-path-circle" :class="{ active: form.fahrenheitResult }">
+                    <p v-if="form.fahrenheitResult">Celsius: {{ form.fahrenheitResult }} ℃</p>
+                    <button @click="clearEverything" class="absolute right-2 top-2 h-4 w-4 z-10" aria-label="Close Results Button">x</button>
+                </div>
+            </UiFormContainer>
+        </UiFormErrorContainer>
     </div>
 </template>
 
@@ -19,8 +30,11 @@ export default {
         return {
             form: {
                 celsius: null,
+                fahrenheit: null,
                 result: null,
+                fahrenheitResult: null,
                 error: "",
+                fahrenheitError: "",
             },
         };
     },
@@ -31,7 +45,15 @@ export default {
                 this.form.errors = "All fields is required";
                 return;
             }
-            this.form.result = (this.form.celsius * 9) / 5 + 32;
+            this.form.result =( (this.form.celsius * 9) / 5 + 32).toFixed(1);
+        },
+        calculateFahrenheit() {
+            this.form.fahrenheitError = "";
+            if (!this.form.fahrenheit) {
+                this.form.fahrenheitError = "All fields is required";
+                return;
+            }
+            this.form.fahrenheitResult = (((this.form.fahrenheit - 32) * 5) / 9).toFixed(1);
         },
         clearEverything() {
             for (let key in this.form) {
--- pages/unit/temperature-converter/index.vue
+++ pages/unit/temperature-converter/index.vue
@@ -1,9 +1,18 @@
 <template>
     <UiBaseContainer>
         <CalculatorsUnitTemperatureCalc />
+        <UiCalcDescription>
+            <template v-slot:description>
+                This page contains two temperature converters that allow you to easily convert between Celsius (°C) and Fahrenheit (°F) temperatures. Simply input the temperature value in either Celsius or Fahrenheit, and the corresponding value in the other unit will be calculated and displayed for
+                you.
+            </template>
+            <template v-slot:example
+                >Let's say you want to convert a temperature of 25°C to Fahrenheit. You would enter "25" in the Celsius input field of the first calculator, click "Calculate", and the corresponding Fahrenheit temperature, which is 77°F, would be displayed. If you wanted to convert the
+                Fahrenheit temperature 75°F to Celsius, you would enter "75" in the Fahrenheit input field of the second calculator, click "Calculate", and the corresponding Celsius temperature, which is 23.9°C, would be displayed.</template
+            >
+        </UiCalcDescription>
     </UiBaseContainer>
 </template>
-
 
 <script setup>
 useHead({
