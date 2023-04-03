249717c603893fdf824fe1b9d0e22bcb5d7330bc
gkas <george.kasnis@gmail.com>
2023-04-03 23:07:03 +0300

    added text to homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,34 @@
 <template>
-    <div class="w-full p-small h-full grid grid-cols-1 lg:grid-cols-3 gap-big">
-        <div class="border-2 border-b brutalist-shadow p-big">
-            <h1 class="text-big font-bold">Fitness</h1>
-            <div class="p-small">
-                <div>
-                    <nuxt-link to="/fitness/bmi-calculator" class="hover:font-bold">BMI Calculator</nuxt-link>
+    <div>
+        <div class="w-full  h-full grid grid-cols-1 lg:grid-cols-3 gap-big">
+            <div class="border-2 border-b brutalist-shadow p-big min-h-[20rem] tablet:min-h-[none]">
+                <h1 class="text-big font-bold">Fitness</h1>
+                <div class="p-small">
+                    <div>
+                        <nuxt-link to="/fitness/bmi-calculator" class="hover:font-bold">BMI Calculator</nuxt-link>
+                    </div>
+                    <div>
+                        <nuxt-link to="/fitness/body-fat-calculator" class="hover:font-bold">Body Fat Calculator</nuxt-link>
+                    </div>
                 </div>
-                <div>
-                    <nuxt-link to="/fitness/body-fat-calculator" class="hover:font-bold">Body Fat Calculator</nuxt-link>
+            </div>
+            <div class="border-2 border-b brutalist-shadow p-big min-h-[20rem]">
+                <h2 class="text-big font-bold">Math</h2>
+                <div class="p-small">
+                    <nuxt-link to="/math/percentage-calculator" class="hover:font-bold">Percentage Calculator</nuxt-link>
+                </div>
+            </div>
+            <div class="border-2 border-b brutalist-shadow p-big min-h-[20rem]">
+                <h2 class="text-big font-bold">Unit</h2>
+                <div class="p-small">
+                    <nuxt-link to="/unit/temperature-converter" class="hover:font-bold">Temperature Converter</nuxt-link>
                 </div>
             </div>
         </div>
-        <div class="border-2 border-b brutalist-shadow p-big">
-            <h2 class="text-big font-bold">Math</h2>
-            <div class="p-small">
-                <nuxt-link to="/math/percentage-calculator" class="hover:font-bold">Percentage Calculator</nuxt-link>
-            </div>
-        </div>
-        <div class="border-2 border-b brutalist-shadow p-big">
-            <h2 class="text-big font-bold">Unit</h2>
-            <div class="p-small">
-                <nuxt-link to="/unit/temperature-converter" class="hover:font-bold">Temperature Converter</nuxt-link>
-            </div>
-        </div>
+        <p class=" border-b brutalist-shadow p-big mt-big">
+            Discover a wide range of powerful and easy-to-use calculators for fitness, math, unit conversions, and more on our site. Our calculators are designed to help you quickly and accurately perform complex calculations, saving you time and effort. Whether you're a student, professional, or
+            enthusiast, our selection of calculators will make your life easier. Start exploring today and see how our calculators can transform the way you work and learn.
+        </p>
     </div>
 </template>
 
