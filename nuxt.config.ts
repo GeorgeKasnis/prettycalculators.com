64fa06d3d36784744dcd4534d8a3ec1fb455e724
gkas <george.kasnis@gmail.com>
2023-04-03 20:54:24 +0300

    wip

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -34,7 +34,7 @@ export default defineNuxtConfig({
                 { name: "google-site-verification", content: "zF5qj09ziYy_arXAmoNcsI7R4HKtGaiDVXA6jJbxqOA" },
                 {
                     property: "og:image",
-                    content: "/og-image.png",
+                    content: "https://www.prettycalculators.com/og-image.png",
                 },
                 {
                     property: "og:image:type",
