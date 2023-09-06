953373e5a26cd49365f64126218daedbb5d68871
gkas <george.kasnis@gmail.com>
2023-09-06 20:43:35 +0300

    wip

--- pages/fitness/index.vue
+++ pages/fitness/index.vue
@@ -11,7 +11,7 @@
         </template>
         <template v-slot:recommended>
             <CategoryCardsMath class="mb-big" />
-            <adsbygoogle ad-slot="1026555560" ad-format="fluid" ad-style="{display:block; text-align:center;}" ad-layout="in-article" />
+            <adsbygoogle ad-slot="1026555560" ad-format="fluid" ad-style="{display:block; text-align:center; width:300px}" ad-layout="in-article" />
             <CategoryCardsUnit class="mb-big" />
             <CategoryCardsOther class="mb-big" />
         </template>
