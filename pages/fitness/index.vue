36161b945c7df65077d66c9fd715deb6a9e50c83
gkas <george.kasnis@gmail.com>
2023-09-09 18:05:41 +0300

    wip

--- pages/fitness/index.vue
+++ pages/fitness/index.vue
@@ -2,18 +2,39 @@
     <UiCategoryContainer>
         <template v-slot:main>
             <CategoryCardsFitness :hasH1="true" class="mb-big" />
+
             <p>
-                Looking to take your fitness routine to the next level? Our fitness calculator tools can help you achieve your goals! Whether you're looking to track your progress, set new goals, or determine the number of calories you need to consume, our calculators can provide you with accurate
-                and reliable results. Our BMI calculator can help you determine your body mass index, which is a measure of body fat based on your height and weight. Our body fat calculator uses your age, gender, height, and weight to determine your body fat percentage, which can be an important
-                indicator of overall health. In addition, our calorie counter can help you track the number of calories you consume each day, while our heart rate monitor can help you optimize your workouts. Our calculators are easy to use, free, and accessible from anywhere. Start using our fitness
-                calculator tools today and take the first step towards achieving your fitness goals!
+                Looking to elevate your fitness journey and achieve your health and wellness goals? Look no further! Our comprehensive suite of fitness calculator tools is here to support you every step of the way. Whether you're eager to monitor your progress, establish ambitious new targets, or
+                gain insight into your nutritional needs, our calculators deliver precise and dependable results.
             </p>
+
+            <p>
+                <nuxt-link to="/fitness/bmi-calculator" class="font-bold">BMI Calculator</nuxt-link>: Calculate your Body Mass Index (BMI), a crucial indicator of body fat based on your height and weight. Understanding your BMI is fundamental to assessing your overall health and can serve as a
+                starting point for your fitness journey.
+            </p>
+
+            <p>
+                <nuxt-link to="/fitness/body-fat-calculator" class="font-bold">Body Fat Calculator</nuxt-link>: Our Body Fat Calculator takes into account your age, gender, height, and weight to determine your body fat percentage. This metric is a vital measure of your overall health and fitness
+                progress, helping you tailor your workouts and nutrition plans effectively.
+            </p>
+
+            <p>
+                <nuxt-link to="/fitness/bmr-calculator" class="font-bold">BMR Calculator</nuxt-link>: Discover your Basal Metabolic Rate (BMR), which reveals the number of calories your body requires at rest. Knowing your BMR is essential for creating a personalized calorie intake plan that supports
+                your fitness goals, whether it's losing weight, maintaining, or bulking up.
+            </p>
+
+            <p>
+                Our user-friendly calculators are completely free and accessible from any device with an internet connection. Start using our fitness calculator tools today to embark on your fitness journey with confidence and purpose. Take the first step towards achieving your fitness goals and
+                enhancing your overall well-being. With our powerful and easy-to-use fitness calculators, success is within your reach!
+            </p>
+
+            <p>Boost your fitness website's visibility and reach by incorporating these essential keywords and SEO-rich content into your description. This will help your page rank higher in search engine results, making it more accessible to users seeking fitness and health-related information.</p>
         </template>
         <template v-slot:recommended>
             <CategoryCardsMath class="mb-big" />
             <adsbygoogle class="mb-big" ad-slot="1026555560" ad-format="fluid" ad-style="display:block !important; text-align:center !important; width:300px !important; margin-left:auto;margin-right:auto;" ad-layout="in-article" />
             <CategoryCardsUnit class="mb-big" />
-              <adsbygoogle class="mb-big" ad-slot="1914672383" ad-format="auto" ad-style="display:block !important; text-align:center !important; width:300px !important; margin-left:auto;margin-right:auto;" ad-layout="in-article" />
+            <adsbygoogle class="mb-big" ad-slot="1914672383" ad-format="auto" ad-style="display:block !important; text-align:center !important; width:300px !important; margin-left:auto;margin-right:auto;" ad-layout="in-article" />
             <CategoryCardsOther class="mb-big" />
         </template>
     </UiCategoryContainer>
