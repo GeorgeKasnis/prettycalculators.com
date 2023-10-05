c0f360ad8ab7716db74918d5f041c91816bd7a53
gkas <george.kasnis@gmail.com>
2023-10-05 23:42:54 +0300

    tritse wishes

--- components/calculators/fitness/BmiCalc.vue
+++ components/calculators/fitness/BmiCalc.vue
@@ -1,10 +1,9 @@
 <template>
     <UiFormErrorContainer :errors="form.errors">
         <UiFormContainer title="BMI Calculator">
-            <InputsTextInput @keyup.enter="calculate" aria-label="Height in cm input to calculate Body Mass Index (BMI)" placeholder="Height" v-model="form.height" measurementUnit="Cm" />
-            <InputsTextInput @keyup.enter="calculate" aria-label="Weight in kg input to calculate Body Mass Index (BMI)" placeholder="Weight" v-model="form.weight" measurementUnit="Kg" />
-            <UiBaseResult v-if="form.bmi" text="Result" :result="form.bmi.toFixed(1)" />
-            <ButtonsCalcBtn @click="calculate" />
+            <InputsTextInput aria-label="Height in cm input to calculate Body Mass Index (BMI)" placeholder="Height" v-model="form.height" measurementUnit="Cm" />
+            <InputsTextInput aria-label="Weight in kg input to calculate Body Mass Index (BMI)" placeholder="Weight" v-model="form.weight" measurementUnit="Kg" />
+            <UiBaseResult text="Result" :result="result" />
             <ButtonsClearBtn @click="clearEverything(form)" />
         </UiFormContainer>
     </UiFormErrorContainer>
@@ -17,62 +16,17 @@ export default {
             form: {
                 height: null,
                 weight: null,
-                bmi: null,
                 errors: "",
             },
         };
     },
     computed: {
-        positiveNumberRule() {
-            return (value) => {
-                if (!value || value <= 0) {
-                    return "Please enter a positive number";
-                }
-                return true;
-            };
-        },
-        requiredRule() {
-            return [(v) => !!v || "This field is required"];
-        },
-        bmiCategory() {
-            let category = "";
-
-            switch (true) {
-                case this.form.bmi < 18.5:
-                    category = "Underweight";
-                    break;
-                case this.form.bmi < 25:
-                    category = "Normal weight";
-                    break;
-                case this.form.bmi < 30:
-                    category = "Overweight";
-                    break;
-                default:
-                    category = "Obese";
-                    break;
-            }
-
-            return category;
-        },
-    },
-    methods: {
-        calculate() {
-            if (!this.form.height || !this.form.weight) {
-                this.form.errors = "All fields is required";
-                this.form.bmi = null;
-                return;
-            }
-
-            if (this.form.height <= 0 || this.form.weight <= 0) {
-                this.form.errors = "Height and weight must be positive numbers";
-                this.form.bmi = null;
-                return;
-            }
-
+        result() {
             const heightInMeters = this.form.height / 100;
             const bmi = this.form.weight / (heightInMeters * heightInMeters);
-            this.form.bmi = bmi;
-            this.form.errors = "";
+            if (bmi > 0 && !isNaN(bmi) && isFinite(bmi)) {
+                return bmi.toFixed(1);
+            }
         },
     },
 };
--- components/ui/BaseResult.vue
+++ components/ui/BaseResult.vue
@@ -2,7 +2,7 @@
     <p v-if="hasHtml" class="mobile:text-center mb-0">
         <slot></slot>
     </p>
-    <p v-else class="mobile:text-center text-lightpurple mb-0"> {{text}} :<span class="text-big  text-center  text-black  ml-small mobile:block mobile:ml-0">{{ result }}</span></p>
+    <p v-else class="mobile:text-center text-lightpurple mb-0"> {{text}} :<span class="text-big  text-center  text-black  ml-small mobile:block mobile:ml-0"><span v-if="result">{{ result }}</span></span></p>
 </template>
 
 <script>
