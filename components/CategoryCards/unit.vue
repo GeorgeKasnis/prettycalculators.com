e5def2524f87f2185e329b5e1b76ead7d78adda7
gkas <george.kasnis@gmail.com>
2023-10-23 19:03:50 +0300

    wip

--- components/CategoryCards/unit.vue
+++ components/CategoryCards/unit.vue
@@ -2,7 +2,7 @@
     <div class="border-2 border-b brutalist-shadow p-big min-h-[20rem]">
         <h1 v-if="hasH1" class=""><nuxt-link class="text-big font-bold" to="/unit">Unit</nuxt-link></h1>
         <h2 v-else class=""><nuxt-link class="text-big font-bold" to="/unit">Unit</nuxt-link></h2>
-        <div class="p-small">
+        <div class="">
             <div>
                 <nuxt-link to="/unit/celsius-to-fahrenheit-converter" class="hover:font-bold">Celsius to Fahrenheit Converter</nuxt-link>
             </div>
