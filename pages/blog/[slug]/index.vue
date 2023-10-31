1ed837f96bf6be433a2715347806ef92d608a673
gkas <george.kasnis@gmail.com>
2023-10-31 21:31:06 +0200

    FIX HYDRATION MISMATCH

--- pages/blog/[slug]/index.vue
+++ pages/blog/[slug]/index.vue
@@ -2,11 +2,7 @@
     <div class="max-w-4xl w-full">
         <h1 class="text-big font-bold mb-small">{{ post.items[0].fields.title }}</h1>
         <img class="w-full aspect-video object-cover" :src="`https://${post.includes.Asset[0].fields.file.url}`" :alt="post.includes.Asset[0].fields.title" />
-        <div class="mt-big">
-            <ClientOnly>
-                <p v-if="documentToHtmlString(post.items[0].fields.content)" v-html="documentToHtmlString(post.items[0].fields.content)"></p>
-            </ClientOnly>
-        </div>
+        <div class="mt-big" v-html="documentToHtmlString(post.items[0].fields.content)"></div>
     </div>
 </template>
 
@@ -16,7 +12,6 @@ import { documentToHtmlString } from "@contentful/rich-text-html-renderer";
 const runtimeConfig = useRuntimeConfig();
 const route = useRoute();
 const { data: post } = await useFetch(`${runtimeConfig.public.API_URL}&content_type=blog&fields.slug=${route.params.slug}`);
-
 
 useHead({
     title: `Pretty Calculators - ${post.value.items[0].fields.metaTitle}`,
