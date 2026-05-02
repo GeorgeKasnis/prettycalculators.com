fbe5aa7674f005acac6077e2af62a44f3a591058
gkas <george.kasnis@gmail.com>
2026-05-02 22:24:23 +0300

    fix google analytics navigation

--- plugins/vue-gtag.client.js
+++ plugins/vue-gtag.client.js
@@ -1,9 +1,10 @@
 import VueGtag from 'vue-gtag-next'
- 
+
 export default defineNuxtPlugin((nuxtApp) => {
+  const router = useRouter()
   nuxtApp.vueApp.use(VueGtag, {
     property: {
       id: 'G-VL24XPE0N4'
     }
-  })
+  }, router)
 })
